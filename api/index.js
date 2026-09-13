149fff8157cd7302078fcc10cfc65c5d3b1ac2d4
jonayed-ahmed-cse <jonayed.stud05@gmail.com>
2026-09-13 17:48:59 +0600

    bug(image size issue): fix image size issue

--- api/index.js
+++ api/index.js
@@ -9,7 +9,9 @@ const taskRoutes = require('../routes/taskRoutes');
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+//app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 app.use('/api/v1', authRoutes);
 app.use('/api/v1', taskRoutes);
